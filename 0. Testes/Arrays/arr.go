88ea220381c8e023587809f5e0b63f2e776c7bfd
Sam Veras <samuel.bernardo.veras@gmail.com>
2024-05-15 23:25:14 -0300

    Testes de arrays dnv

--- 0. Testes/Arrays/arr.go
+++ 0. Testes/Arrays/arr.go
@@ -13,4 +13,28 @@ func main() {
 	p := [3]int{9, 7, 5}
 	fmt.Println("p:", p)
 
+	k := [...]int{21, 22, 23, 24, 29, 31}
+	fmt.Printf("%T\n", k)
+
+	l := [...]int{1: 23, 9: 1, 12: 22}
+	fmt.Println(l)
+
+	tt := [3][3]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+
+	fmt.Println(tt)
+	fmt.Printf("%T\n", tt)
+
+	var lm [2][2]int
+	for i := range 2 {
+		for j := range 2 {
+			lm[i][j] = i + j
+		}
+	}
+
+	fmt.Println(lm)
+
 }
