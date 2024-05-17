bcd9f8f02141410b7d5188d0d1ab92437557dc47
Sam Veras <samuel.bernardo.veras@gmail.com>
2024-05-16 23:21:26 -0300

    Sorting

---
+++ 4. Sorting/sort.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+func BubbleSort(array []int) {
+	done := false
+	n := len(array)
+
+	for !done {
+		done = true
+		for i := 0; i < n-1; i++ {
+			if array[i] > array[i+1] {
+				array[i], array[i+1] = array[i+1], array[i]
+				done = false
+			}
+		}
+	}
+}
+
+func InsertionSort(array []int) {
+	n := len(array)
+	for i := 1; i < n; i++ {
+		for j := i; j >= 1 && array[j] < array[j-1]; j-- {
+			array[j], array[j-1] = array[j-1], array[j]
+		}
+	}
+}
+
+func SelectionSort(array []int) {
+	n := len(array)
+	for i := 0; i < n-1; i++ {
+		min_i := i
+		for j := i + 1; j < n; j++ {
+			if array[j] < array[min_i] {
+				min_i = j
+			}
+		}
+
+		if min_i != i {
+			array[i], array[min_i] = array[min_i], array[i]
+		}
+	}
+}
+
+func MergeSort(array []int) {
+
+}
+func main() {
+	n := []int{23, 4, 67, 9, 1221, 4, 80, 65, 33, 648, 90, 67, 8, 17, 64, 92, 11, 51, 172}
+	fmt.Println("Original:", n)
+	SelectionSort(n)
+
+	fmt.Println("Sorted:  ", n)
+
+}
